3831a0d10c6554211345a0c715752344f4fce315
GLScene <pavelvvassiliev@gmail.com>
2022-12-14 01:24:08 +0300

    Changed SetGLSceneMediaDir() to GetCurrentAssetPath()

---
+++ Asset/Shader/OIT_vtx.glsl
@@ -0,0 +1,5 @@
+void main()
+{
+   gl_Position = vec4(sign(gl_Vertex.xy), -1., 1.);
+   gl_TexCoord[0] = gl_MultiTexCoord0;
+}
--- Demos/media/Shaders/OIT_vtx.glsl
+++
@@ -1,5 +0,0 @@
-void main()
-{
-   gl_Position = vec4(sign(gl_Vertex.xy), -1., 1.);
-   gl_TexCoord[0] = gl_MultiTexCoord0;
-}
