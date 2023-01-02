137cce7d02c1527af48f58ba39eb57871e9a0866
GLScene <pavelvvassiliev@gmail.com>
2023-01-02 23:08:35 +0300

    Renamed clean.cmd for release

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # GLScene
 GLScene is a graphics platform based on OpenGL and VCL components for Delphi & C++ Builder. 
 It provides the class library and tools allowing rendering 3D scenes with spatial objects 
-and smart virtual actors for win32/win64 applications. 
+and smart virtual actors for Win32/Win64 applications. 
 Additional support of CG, FMOD, BASS, ODE, NGD, CUDA/OpenCL, GR32, SDL2 
 TensorFlow Lite and Tetgen is included.
 Supported GPU parallel computing using CUDA/OpenCL components. 
+
+This is the official GitHub fork of the original SourceForge repository. The latter can still be found at http://sf.net/projects/glscene
+
 GLS Team
--- _cleanscene.cmd
+++ _cleanscene.cmd
@@ -1,16 +1,19 @@
-@echo off
+@echo off delete exe and xml docs
 
-rem delete packages and libs
+del *.exe /s  
+del *.scr /s
+del *.xml /s
 
-del *.bpl /s
-del *.dcp /s
-del *.hpp /s
-del *.bpi /s
-del *.lib /s
+@echo off delete delphi units
 del *.dcu /s
+del *.bak /s
 
-rem delete debug files
+@echo off delete C++ includes and libs
+rem del *.hpp /s
+rem del *.bpi /s
+rem del *.lib /s
 
+@echo off delete debug files
 del *.ddp /s
 del *.ppu /s
 del *.o /s
@@ -27,17 +30,15 @@ del *.map /s
 del *.drc /s
 del *.local /s
 
-rem delete more files
+@echo off delete more rad studio files
 
-del *.bak /s
-del *.xml /s
 del *.identcache /s
 del *.otares /s
 del *.tvsconfig /s
 del *.stat /s
 del *.db /s
 
-rem delete more cpp builder files
+@echo off delete more cpp builder files
 
 del *.#00 /s
 del *.pch /s
@@ -49,26 +50,28 @@ del *.ils /s
 del *.pdi /s
 del *.vlb /s
 
-
-echo _
 echo ************************************************
 echo             Don't delete some files
 echo ************************************************
-echo _
 
 attrib +R "AdvDemos/Q3Demo/Model/animation.cfg"
-rem del *.cfg /s  - there are quake's animations
+rem del *.cfg /s - there are quake's animations
 attrib -R "AdvDemos/Q3Demo/Model/animation.cfg"
 
 del *.res /s
 rem del *.ico /s  - some projects have own icos
 rem del *.cur /s  - cursors
-rem del *.obj /s  - obj models in media and resources for lazarus
+rem del *.obj /s  - obj models in assets or resources for lazarus
 
+echo---------------------------------------------------------
 
 echo delete all .svn directories with subdirectories and files 
 for /r %1 %%R in (.svn) do if exist "%%R" (rd /s /q "%%R")
+echo delete all .git directories with subdirectories and files 
+for /r %1 %%R in (.git) do if exist "%%R" (rd /s /q "%%R")
+
 echo---------------------------------------------------------
+
 echo delete debug and Platform directories with all subdirectories and files 
 rem    for /r %1 %%R in (Win32) do if exist "%%R" (rd /s /q "%%R")
 rem    for /r %1 %%R in (Win64) do if exist "%%R" (rd /s /q "%%R")
@@ -79,4 +82,3 @@ for /r %1 %%R in (Release) do if exist "%%R" (rd /s /q "%%R")
 for /r %1 %%R in (__history) do if exist "%%R" (rd /s /q "%%R")
 for /r %1 %%R in (__recovery) do if exist "%%R" (rd /s /q "%%R")
 for /r %1 %%R in (__astcache) do if exist "%%R" (rd /s /q "%%R")
-for /r %1 %%R in (staticobjs) do if exist "%%R" (rd /s /q "%%R")
